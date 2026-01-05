758b2e4cd99a5d6f5cc54fca843ea09d2219f06c
vrajsavaniya927-create <vrajsavaniya927@gmail.com>
2026-01-05 11:37:46 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,30 @@
+#int
+x=20
+print(x)
+#str
+a = "vrajsavaniya"
+print(a)
+#float
+b = 3.88
+print(b)
+#boolean
+p = True
+print(p)
+#tuple
+i = ("apple","ball","catch")
+print(i)
+#list
+list =["apple","ball","catch"]
+print(list)
+#dict
+d = {"name":"vraj","age":"18"}
+print(d)
+#8.complex
+"""k = ij
+print(k)"""
+#9.range
+v = range(5)
+print(v)
+#10.set
+y = {"apple","ball","catch"}
+print(y)
