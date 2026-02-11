a918e1f637c86221a511acef86cf16fa34d4f9ec
vrajsavaniya927-create <vrajsavaniya927@gmail.com>
2026-02-11 08:15:05 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,39 @@
+"""age=int(input("enter your age..."))
+if age>=18:
+    print("you can vote..!")
+else:
+    print("you can vote..!")  
+    
+#grade
+marks = int(input("enter your mark"))
+if marks>=90:
+    print("you got grade A")
+elif marks>=80:
+    print("you got grade B ")
+else:
+    print("you are fail")  
+
+#positive negative
+number = int(input("enter number"))
+if number==0:
+    print("number is zero")
+elif number>0:
+    print("number is positive")
+else:
+    print("number is negative") 
+    
+#license 
+    
+age=int(input("enter your age..."))
+if age>=18:
+    print("you are eligible..!")
+else:
+    print("you are not eligible..!")
+    
+#odd or even
+
+num=int(input("enter number"))
+if num%2==0:
+    print("number is even")
+else:
+    print("number is odd")"""
