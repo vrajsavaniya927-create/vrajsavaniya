47e70a22ebf7090b9a492de12c4eed97860ca5e0
vrajsavaniya927-create <vrajsavaniya927@gmail.com>
2026-02-28 08:52:54 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,23 @@
+"""i=1
+while i<=5:
+   print(i)
+   i=i+1"""
+
+"""n = int(input("enter number ="))
+sum=0
+i =1
+while i<=n:
+        sum = sum+i
+        i=i+1
+print (sum)"""
+
+
+"""num = int(input("enter number you want its table"))
+i=1
+while i<= 10:
+    print(num,"x",i,"=",num*i)
+    i=i+1"""
+
+
+
+
