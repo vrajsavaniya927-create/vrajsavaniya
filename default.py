791f3607cd863af12b78c2a2ee7c4c0665d2110e
vrajsavaniya927-create <vrajsavaniya927@gmail.com>
2026-03-18 08:27:29 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,13 @@
+
+"""def greet(name="guest"):
+    print("hello",name)
+    
+greet("alice")
+greet()
+
+def add(a,b=5):
+    print("sum",a+b)
+    
+add(10,20)
+add(10)"""
+
---
+++ function.py
@@ -0,0 +1,54 @@
+"""def  add(a,b):
+    print("a=",a)
+    print("b=",b)
+    return a+b
+result=add(2,6)
+print("sum=",result)
+
+def student_info(name,roll,marks):
+    print("name:",name)
+    print("roll no:",roll)
+    print("marks:",marks)
+student_info("ravi",101,85)
+
+def simple_interest(p,r,n):
+        si=(p*r*n)/100
+        print("simple interest : ",si)
+simple_interest(10000,2,2)
+simple_interest(50000,1,2)
+
+def ar_circle(r):
+    area=3.14*r*r
+    print("area of circle:",area)
+ar_circle(1.5)
+ar_circle(4)
+
+def check_value(no):
+    if(no>0):
+        print("positive")
+    elif(no<0):
+        print("negative")
+    else:
+        print("zero")
+check_value(0)
+check_value(90)
+check_value(-10)
+
+def odd_even(no):
+    if(no%2==0):
+        print(f"value {no} is even")
+    else:
+        print(f"value {no} is odd")
+odd_even(50)
+odd_even(15)
+
+def addition(a,b):
+    add=a+b
+    print("addition of two values",add)
+    sub=a-b
+    print("subtraction of two values",sub)
+    multi=a*b
+    print("multiplication of two values",multi)
+    div=a/b
+    print("division of two values",div)   
+addition(50,10.5)"""
